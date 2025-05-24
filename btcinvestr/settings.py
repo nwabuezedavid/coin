3156e0c40dbce4039ab4b4d630b64f053df79810
NwabuezeDavid <davidnwabueze333@gmail.com>
2025-05-24 07:26:20 -0400

    first commit

--- btcinvestr/settings.py
+++ btcinvestr/settings.py
@@ -94,13 +94,19 @@ WSGI_APPLICATION = 'btcinvestr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+from urllib.parse import urlparse
+tmpPostgres = urlparse('postgresql://neondb_owner:npg_sMaQ7x3gfNVX@ep-mute-dawn-a8hwii60-pooler.eastus2.azure.neon.tech/neondb?sslmode=require')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
